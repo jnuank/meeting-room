686c6f2518b346e95cc408a0bec43a6f0a123891
Jun Nakajima <ikuta1919@gmail.com>
2019-12-31 16:46:31 +0900

    update: 会議室値オブジェクトをEnumにする

--- Model/Rooms.cs
+++ Model/Rooms.cs
@@ -3,10 +3,16 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace modeling_mtg_room.Model
 {
+    /// <summary>
+    /// 会議室オブジェクト
+    /// </summary>
     public enum Rooms
     {
+        // 今の所、何かしら振る舞いを持たせるイメージが付かないので、Enumにしている
         A,
         B,
-        C
+        Reception,
+        Frist,
+        Second,
     }
 }
