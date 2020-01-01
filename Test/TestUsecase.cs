4449b1e88b9206cbb5b65624b87e59f2addb86f7
Jun Nakajima <ikuta1919@gmail.com>
2020-01-01 18:28:41 +0900

    update: usecase層をstaticキーワードを外す

--- Test/TestUsecase.cs
+++ Test/TestUsecase.cs
@@ -1,7 +1,7 @@
 using System;
 using Xunit;
-using modeling_mtg_room.Usecase;
 using modeling_mtg_room.Model;
+using modeling_mtg_room.Usecase;
 using Moq;
 
 namespace modeling_mtg_room.Test
@@ -11,6 +11,9 @@ namespace modeling_mtg_room.Test
         [Fact]
         public void 会議室を予約したら予約が発生する()
         {
+            //todo: ここでテスト用のInMemoryリポジトリを入れる
+
+            
             var dateTime = new Mock<IDateTime>();
             dateTime.Setup(d => d.Now)
                 .Returns(new DateTime(2020, 1, 20, 0,0,0));
@@ -18,18 +21,23 @@ namespace modeling_mtg_room.Test
             string room = "a";
             int reserverOfNumber = 5;
             string reserverId = "abcdefg";
-            Reserve よやく = Usecase.Usecase.ReserveMeetingRoom(room,
+
+            var usecase = new ReserveApplication(dateTime.Object);
+            Reserve よやく = usecase.ReserveMeetingRoom(room,
                                                         2020, 2, 1, 10, 0,
                                                         2020, 2, 1, 13, 15,
                                                         reserverOfNumber,
-                                                        reserverId,
-                                                        dateTime.Object);
+                                                        reserverId);
             Assert.NotNull(よやく);
             Assert.Equal(MeetingRooms.A, よやく.Room);
             Assert.Equal("2020/02/01 10:00:00", よやく.TimeSpan._start.Value.ToString("yyyy/MM/dd HH:mm:ss"));
             Assert.Equal("2020/02/01 13:15:00", よやく.TimeSpan._end.Value.ToString("yyyy/MM/dd HH:mm:ss"));
             Assert.Equal(5, よやく.ReserverOfNumber.Value);
             Assert.Equal("abcdefg", よやく.ReserverId.Value);
+
+
+            
         }
     }
+    // 被ってしまったらエラーとするテストを見る
 }
--- Usecase/Usecase.cs
+++ Usecase/Usecase.cs
@@ -3,28 +3,27 @@ using modeling_mtg_room.Model;
 
 namespace modeling_mtg_room.Usecase
 {
-    public class Usecase
+    public class ReserveApplication
     {
         private readonly IDateTime dateTime;
         
-        private Usecase(IDateTime dateTime = null)
+        public ReserveApplication(IDateTime dateTime = null)
         {
             // デフォルトではサーバが保持する時間を使用する
             this.dateTime = dateTime ?? new ServerDateTime();
         }
-        public static Reserve ReserveMeetingRoom(string room,
+        public Reserve ReserveMeetingRoom(string room,
                                             int startYear, int startMonth, int startDay, int startHour, int startMinute,
                                             int endYear, int endMonth, int endDay, int endHour, int endMinute,
                                             int reserverOfNumber,
-                                            string reserverId,
-                                            IDateTime dateTime = null)
+                                            string reserverId)
         {
             MeetingRooms mtgRoom;
             if(!Enum.TryParse(room, true, out mtgRoom))
                 throw new ApplicationException("指定された会議室が存在しません");
 
-            var startTime = new ReservedTime(startYear, startMonth, startDay, startHour, startMinute, dateTime);
-            var endTime = new ReservedTime(endYear, endMonth, endDay, endHour, endMinute, dateTime);
+            var startTime = new ReservedTime(startYear, startMonth, startDay, startHour, startMinute, this.dateTime);
+            var endTime = new ReservedTime(endYear, endMonth, endDay, endHour, endMinute, this.dateTime);
             var timeSpan = new ReservedTimeSpan(startTime, endTime);
             var reserver = new ReserverOfNumber(reserverOfNumber);
             var id = new ReserverId(reserverId);
