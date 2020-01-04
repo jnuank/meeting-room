e1ffe0fc666d6648229409ef2405041ca9d3c276
Jun Nakajima <ikuta1919@gmail.com>
2020-01-04 18:41:47 +0900

    update: API gateway経由でユースケースを呼び出すことができる

--- Lambda/Handler.cs
+++ Lambda/Handler.cs
@@ -2,6 +2,9 @@ using Amazon.Lambda.Core;
 using System;
 using modeling_mtg_room.Domain.Reserve;
 using InMemoryInfrastructure;
+using Newtonsoft.Json;
+using System.Net;
+using System.Collections.Generic;
 
 [assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -9,23 +12,60 @@ namespace AwsDotnetCsharp
 {
     public class Handler
     {
-      public Response Hello(Request request)
+      public LambdaResponse Hello(Request request)
       {
-          DateTime start = DateTime.Parse(request.StartDateTime, null, System.Globalization.DateTimeStyles.RoundtripKind);
-          DateTime end   = DateTime.Parse(request.EndDateTime  , null, System.Globalization.DateTimeStyles.RoundtripKind);
+          // yyyy-mm-ddThh:MM:ss+zz:zz形式を、DateTime型にParseする
+          // DateTime start = DateTime.Parse(request.StartDateTime, null, System.Globalization.DateTimeStyles.RoundtripKind);
+          // DateTime end   = DateTime.Parse(request.EndDateTime  , null, System.Globalization.DateTimeStyles.RoundtripKind);
+
+          DateTime start = DateTime.Parse("2020-1-10T10:00:40+09:00", null, System.Globalization.DateTimeStyles.RoundtripKind);
+          DateTime end   = DateTime.Parse("2020-1-10T18:45:40+09:00" , null, System.Globalization.DateTimeStyles.RoundtripKind);
 
           var repository = new InMemoryReserveRepository();
           // ユースケースを実行する
           var usecase = new ReserveApplication(repository);
-          var id = usecase.ReserveMeetingRoom(request.Room,
+          var id = usecase.ReserveMeetingRoom("A",
                                               start.Year,start.Month,start.Day,start.Hour,start.Minute,
                                               end.Year,end.Month,end.Day,end.Hour,end.Minute,
-                                              int.Parse(request.ReserverOfNumber),
-                                              request.ReserverId);
+                                              2,
+                                              "abc");
+          // var id = usecase.ReserveMeetingRoom(request.Room,
+          //                                     start.Year,start.Month,start.Day,start.Hour,start.Minute,
+          //                                     end.Year,end.Month,end.Day,end.Hour,end.Minute,
+          //                                     int.Parse(request.ReserverOfNumber),
+          //                                     request.ReserverId);
           Reserve reserve = repository.Find(id);
-          return new Response(reserve.Room.ToString(), request);
+          return new LambdaResponse {
+              StatusCode = HttpStatusCode.OK,
+              Headers = null,
+              Body = JsonConvert.SerializeObject(
+                  new ResponseParam 
+                  {
+                      Romm = reserve.Room.ToString()
+                  })
+          };
+          //return new Response(reserve.Room.ToString(), request);
       }
     }
+
+    public class LambdaResponse
+    {
+        [JsonProperty(PropertyName = "statusCode")]
+        public HttpStatusCode StatusCode { get; set; }
+
+        [JsonProperty(PropertyName = "headers")]
+        public Dictionary<string, string> Headers { get; set; }
+
+        [JsonProperty(PropertyName = "body")]
+        public string Body { get; set; }
+    }
+
+    public class ResponseParam
+    {
+        [JsonProperty(PropertyName = "room")]
+        public string Romm { get; set; }
+    }
+
     public class Response
     {
       public string Message {get; set;}
